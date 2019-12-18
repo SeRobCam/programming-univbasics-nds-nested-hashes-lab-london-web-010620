dbf30d63f073b42da5957af65da50408d23debb9
Sergio Robles <sroblesc@gmail.com>
2019-12-18 01:49:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,16 +16,21 @@ end
 
 #2:
 # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+# held in the constants
 
 def literal_aoh
  
 end
 
 def aoh_lookup(aoh, row, key)
+  
 end
 
+
+#3:
+# Update the AoH data at row and key to have the value of new_value
+# Return the updated AoH
+
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
+  
 end
